9fff5805830e01b5f8095b957f34383762c09a7c
Ricardo Piccoli <ricardo.piccoli@hp.com>
2019-01-10 15:52:20 -0200

    Fix code for day 5

--- day05.py
+++ day05.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python3
 from string import ascii_lowercase
 
-polymer = list(map(ord, input().strip()))
+def collapsed(remove_unit = None):
+    p = [ u for u in polymer if tolower(u) != remove_unit ]
+    i = 1
+    while i < len(p):
+        while 0 < i < len(p) and polarized(p, i):
+            del p[i - 1:i + 1]
+            i -= 1
+        i += 1
+
+    return p
 
 def polarized(p, i):
     return abs(p[i] - p[i - 1]) == 32
@@ -9,16 +18,7 @@ def polarized(p, i):
 def tolower(c):
     return c + 32 if c < 97 else c
 
-def collapsed(remove_unit = None):
-    p = [ u for u in polymer if tolower(u) != remove_unit ]
-    i = 1
-    while i < len(p):
-        while 0 < i < len(p) and polarized(p, i):
-            p[i - 1:] = p[i + 1:]
-            i -= 1
-        i += 1
-
-    return p
+polymer = list(map(ord, input().strip()))
 
 polymer = collapsed()
 
