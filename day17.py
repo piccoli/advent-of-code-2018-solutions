40a7287fa5db2f0684cc059299d5adecb3e98484
Ricardo Piccoli <ricardo.piccoli@hp.com>
2019-01-10 14:16:42 -0200

    Replace day 17's solution with a recursive approach

--- day17.py
+++ day17.py
@@ -2,12 +2,50 @@
 import sys, re
 
 from functools import partial
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 Area = namedtuple('Area', 'x0 y0 x1 y1')
 
+Empty, Clay, Flowing, Settled = ' #|~'
+Solids = Clay + Settled
+
+def fill(x, y):
+    reservoir[x, y] = Flowing
+
+    if y == scan.y1:
+        return
+
+    if reservoir[x, y + 1] == Empty:
+        fill(x, y + 1)
+
+    if reservoir[x, y + 1] == Flowing:
+        return
+
+    x0 = x - 1
+    while reservoir[x0, y + 1] in Solids\
+        and reservoir[x0, y] not in Solids:
+        reservoir[x0, y] = Flowing
+        x0 -= 1
+
+    x1 = x + 1
+    while reservoir[x1, y + 1] in Solids\
+        and reservoir[x1, y] not in Solids:
+        reservoir[x1, y] = Flowing
+        x1 += 1
+
+    if reservoir[x0, y] == Empty:
+        fill(x0, y)
+
+    if reservoir[x1, y] == Empty:
+        fill(x1, y)
+
+    if reservoir[x0, y] in Solids and reservoir[x1, y] in Solids:
+        reservoir.update({
+            (x, y): Settled for x in range(x0 + 1, x1)
+        })
+
 def read():
-    reservoir = set()
+    reservoir = defaultdict(lambda: Empty)
     parse_record = partial(re.match, r'([xy])=(\d+),\s*([xy])=(\d+)\.\.(\d+)')
 
     records = map(parse_record, sys.stdin.readlines())
@@ -17,10 +55,10 @@ def read():
 
         x, y1, y2 = map(int, map(record.group, (2, 4, 5)))
 
-        if vertical:
-            reservoir.update((x, y) for y in range(y1, y2 + 1))
-        else:
-            reservoir.update((y, x) for y in range(y1, y2 + 1))
+        r    = { (x, y): Clay for y in range(y1, y2 + 1) } if vertical\
+          else { (y, x): Clay for y in range(y1, y2 + 1) }
+
+        reservoir.update(r)
 
     area = Area(
         x0 = min(x for x, y in reservoir),
@@ -32,53 +70,20 @@ def read():
     return reservoir, area
 
 reservoir, scan = read()
-water_sources   = { (500, 0) }
-flowing         = set()
-settled_water   = set()
-parent          = { (500, 0): (None, None) }
 
-# TODO Recursive implementation
-while water_sources:
-    solids  = reservoir | settled_water
-    sources = sorted(water_sources, reverse = True)
+max_water = (scan.y1 - scan.y0 + 1) * (scan.x1 - scan.x0 + 1)
+sys.setrecursionlimit(max_water)
 
-    water_sources.clear()
+fill(500, 0)
 
-    for x, y in sources:
-        if y == scan.y1:
-            flowing.add((x, y))
-            continue
+reservoir = {
+    (x, y): reservoir[x, y]
+        for x, y in reservoir
+            if y >= scan.y0
+}
 
-        if (x, y + 1) not in solids:
-            flowing.add((x, y))
-            water_sources.add((x, y + 1))
-            parent[x, y + 1] = x, y
-            continue
+total_settled = sum(reservoir[x, y] == Settled for x, y in reservoir)
+total_flowing = sum(reservoir[x, y] == Flowing for x, y in reservoir)
 
-        x0 = x1 = x
-        while (x0, y + 1) in solids and (x0, y) not in solids:
-            x0 -= 1
-        while (x1, y + 1) in solids and (x1, y) not in solids:
-            x1 += 1
-
-        if (x0, y) in solids and (x1, y) in solids:
-            settled_water.update((xi, y) for xi in range(x0 + 1, x1))
-            flowing.difference_update((xi, y) for xi in range(x0 + 1, x1))
-
-            water_sources.add(parent[x, y])
-        else:
-            if (x0, y) not in solids:
-                water_sources.add((x0, y))
-                parent[x0, y] = x, y
-
-            if (x1, y) not in solids:
-                water_sources.add((x1, y))
-                parent[x1, y] = x, y
-
-            flowing.update((x, y) for x in range(x0 + 1, x1))
-
-total         = list(filter(lambda p: p[1] >= scan.y0, settled_water | flowing))
-total_settled = list(filter(lambda p: p[1] >= scan.y0, settled_water))
-
-print(len(total))
-print(len(total_settled))
+print(total_settled + total_flowing)
+print(total_settled)
--- day17_render.py
+++ day17_render.py
@@ -1,20 +1,61 @@
 #! /usr/bin/env python3
 import sys, re
+
 import numpy as np
 import cv2
 
 from functools import partial
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 Area = namedtuple('Area', 'x0 y0 x1 y1')
 
-CLAY_COLOR   = np.array([ 0  , 114, 255 ])
-FLOW_COLOR   = np.array([ 255, 231, 212 ])
-SOURCE_COLOR = np.array([ 255, 0  , 0   ])
-WATER_COLOR  = np.array([ 255, 115, 0   ])
+Empty, Clay, Flowing, Settled = ' #|~'
+Solids = Clay + Settled
+
+ClayColor   = np.array([ 0  , 114, 255 ])
+FlowColor   = np.array([ 255, 231, 212 ])
+WaterColor  = np.array([ 255, 115, 0   ])
+EmptyColor  = np.array([ 0  , 49 , 109 ])
+
+def fill(x, y):
+    reservoir[x, y] = Flowing
+    render_frame(x, y)
+
+    if y == scan.y1:
+        return
+
+    if reservoir[x, y + 1] == Empty:
+        fill(x, y + 1)
+
+    if reservoir[x, y + 1] == Flowing:
+        return
+
+    x0 = x - 1
+    while reservoir[x0, y + 1] in Solids\
+        and reservoir[x0, y] not in Solids:
+        reservoir[x0, y] = Flowing
+        x0 -= 1
+
+    x1 = x + 1
+    while reservoir[x1, y + 1] in Solids\
+        and reservoir[x1, y] not in Solids:
+        reservoir[x1, y] = Flowing
+        x1 += 1
+
+    if reservoir[x0, y] == Empty:
+        fill(x0, y)
+
+    if reservoir[x1, y] == Empty:
+        fill(x1, y)
+
+    if reservoir[x0, y] in Solids and reservoir[x1, y] in Solids:
+        reservoir.update({
+            (x, y): Settled for x in range(x0 + 1, x1)
+        })
+        render_frame(x, y)
 
 def read():
-    reservoir = set()
+    reservoir = defaultdict(lambda: Empty)
     parse_record = partial(re.match, r'([xy])=(\d+),\s*([xy])=(\d+)\.\.(\d+)')
 
     records = map(parse_record, sys.stdin.readlines())
@@ -24,10 +65,10 @@ def read():
 
         x, y1, y2 = map(int, map(record.group, (2, 4, 5)))
 
-        if vertical:
-            reservoir.update((x, y) for y in range(y1, y2 + 1))
-        else:
-            reservoir.update((y, x) for y in range(y1, y2 + 1))
+        r    = { (x, y): Clay for y in range(y1, y2 + 1) } if vertical\
+          else { (y, x): Clay for y in range(y1, y2 + 1) }
+
+        reservoir.update(r)
 
     area = Area(
         x0 = min(x for x, y in reservoir),
@@ -38,11 +79,38 @@ def read():
 
     return reservoir, area
 
-def render_set(im, window, point_set, color):
-    if not point_set:
+def render_frame(x, y):
+    clay    = [ (x, y) for (x, y), t in reservoir.items() if t == Clay    ]
+    settled = [ (x, y) for (x, y), t in reservoir.items() if t == Settled ]
+    flowing = [ (x, y) for (x, y), t in reservoir.items() if t == Flowing ]
+
+
+    x1 = x + size // 2 - 1
+    x0 = x1 - size
+    y1 = y + size // 10 - 1
+    y0 = y1 - size
+
+    window = Area(
+        x0 = x0,
+        y0 = y0,
+        x1 = x1,
+        y1 = y1
+    )
+
+    im = np.zeros((size, size, 3), np.uint8)
+    im[:, :] = EmptyColor
+
+    render_object(im, window, clay   , ClayColor )
+    render_object(im, window, settled, WaterColor)
+    render_object(im, window, flowing, FlowColor )
+
+    video.write(im)
+
+def render_object(im, window, obj, color):
+    if not obj:
         return
 
-    xs, ys = map(np.int32, zip(*point_set))
+    xs, ys = map(np.int32, zip(*obj))
 
     bounds = (window.y0 <= ys) & (ys < window.y1)\
            & (window.x0 <= xs) & (xs < window.x1)
@@ -52,89 +120,27 @@ def render_set(im, window, point_set, color):
 
     im[ys, xs, :] = color
 
-def render_frame():
-    if not water_sources:
-        return
-
-    y1 = min(y for x, y in water_sources) + size // 2 - 1
-    y0 = y1 - size
-
-    window = Area(
-        x0 = scan.x0,
-        y0 = y0,
-        x1 = scan.x1,
-        y1 = y1
-    )
-
-    im = np.zeros((size, size, 3), np.uint8)
-
-    im[:, :, 0] = 0
-    im[:, :, 1] = 49
-    im[:, :, 2] = 109
-
-    render_set(im, window, reservoir    , CLAY_COLOR  )
-    render_set(im, window, settled_water, WATER_COLOR )
-    render_set(im, window, flowing      , FLOW_COLOR  )
-    render_set(im, window, water_sources, SOURCE_COLOR)
-
-    video.write(im)
-
 reservoir, scan = read()
-water_sources   = { (500, 0) }
-flowing         = set()
-settled_water   = set()
-parent          = { (500, 0): (None, None) }
 
-size = scan.x1 - scan.x0 + 1
+max_water = (scan.y1 - scan.y0 + 1) * (scan.x1 - scan.x0 + 1)
+sys.setrecursionlimit(max_water)
 
+size    = scan.x1 - scan.x0 + 1
 four_cc = cv2.VideoWriter_fourcc(*'MP42')
 video   = cv2.VideoWriter('output.avi', four_cc, 60.0, (size, size))
 
-while water_sources:
-    solids  = reservoir | settled_water
-    sources = sorted(water_sources, reverse = True)
-
-    water_sources.clear()
-
-    for x, y in sources:
-        if y == scan.y1:
-            flowing.add((x, y))
-            continue
-
-        if (x, y + 1) not in solids:
-            flowing.add((x, y))
-            water_sources.add((x, y + 1))
-            parent[x, y + 1] = x, y
-            continue
-
-        x0 = x1 = x
-        while (x0, y + 1) in solids and (x0, y) not in solids:
-            x0 -= 1
-        while (x1, y + 1) in solids and (x1, y) not in solids:
-            x1 += 1
-
-        if (x0, y) in solids and (x1, y) in solids:
-            settled_water.update((xi, y) for xi in range(x0 + 1, x1))
-            flowing.difference_update((xi, y) for xi in range(x0 + 1, x1))
-
-            water_sources.add(parent[x, y])
-        else:
-            if (x0, y) not in solids:
-                water_sources.add((x0, y))
-                parent[x0, y] = x, y
-
-            if (x1, y) not in solids:
-                water_sources.add((x1, y))
-                parent[x1, y] = x, y
-
-            flowing.update((x, y) for x in range(x0 + 1, x1))
-
-    render_frame()
+fill(500, 0)
 
 video.release()
 
-total         = list(filter(lambda p: p[1] >= scan.y0, settled_water | flowing))
-total_settled = list(filter(lambda p: p[1] >= scan.y0, settled_water))
+reservoir = {
+    (x, y): reservoir[x, y]
+        for x, y in reservoir
+            if y >= scan.y0
+}
 
-print(len(total))
-print(len(total_settled))
+total_settled = sum(reservoir[x, y] == Settled for x, y in reservoir)
+total_flowing = sum(reservoir[x, y] == Flowing for x, y in reservoir)
+
+print(total_settled + total_flowing)
+print(total_settled)
